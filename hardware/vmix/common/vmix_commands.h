bda3484554db0482939c68c0df84d25c45610fb0
mirdejavel <mirdejavel@a336a6c2-2142-11df-970b-617f64f7e5c1>
2010-03-15 07:56:18 +0000

    Import of original vmix firmware

---
+++ hardware/vmix/common/vmix_commands.h
@@ -0,0 +1,13 @@
+
+// ==============================================================================
+// USB Commands
+// ------------------------------------------------------------------------------
+
+#define cmd_ECHO			100
+#define cmd_GET				101
+#define cmd_SET_VALUE		104
+#define cmd_SET_SMOOTH		109
+#define cmd_STICK			105
+#define cmd_PRESET_STORE	120
+#define cmd_PRESET_LOAD		125
+
