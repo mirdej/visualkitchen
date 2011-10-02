6a184f298ff4fb5f1ffdd9540d4ce617653ea49b
mirdejavel <mirdejavel@a336a6c2-2142-11df-970b-617f64f7e5c1>
2011-10-02 15:02:25 +0000

    added buildscripts (adapted from videobass) and icon etc

---
+++ max5_GL/building/finish_build.sh
@@ -0,0 +1,25 @@
+# COPY Shaders ----------------------------------------------------------------------------------------------------
+mkdir VisualKitchen.app/Contents/Resources/shaders/
+
+cp ../shaders/*.jxs VisualKitchen.app/Contents/Resources/shaders
+
+# some shared shaders have to be included as well
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/composite/co.normal.jxs VisualKitchen.app/Contents/Resources/shaders/
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/color/cc.brcosa.jxs VisualKitchen.app/Contents/Resources/shaders/
+cp /Applications/Max5/Cycling\ \'74/jitter-shaders/shared/glsl/sh.passthru.xform.vp.glsl VisualKitchen.app/Contents/Resources/shaders/
+
+# COPY HID Usage tables for hi.in
+cp ../mxo/hi.tools/hi.usage* VisualKitchen.app/Contents/Resources/
+
+# jit.openexr.mxo is needed by jit.net.send
+cp -R /Applications/Max5/Cycling\ \'74/jitter-externals/jit.openexr.mxo VisualKitchen.app/Contents/Resources/
+
+# java classes and max.jar is needed by mxj
+cp /Applications/Max5/Cycling\ \'74/java/lib/max.jar VisualKitchen.app/Contents/Resources/
+cp /Applications/Max5/Cycling\ \'74/java/lib/jitter.jar VisualKitchen.app/Contents/Resources/
+cp -R /Applications/Max5/Cycling\ \'74/java/classes VisualKitchen.app/Contents/
+
+# ICON And Plist-----------------------------------------------------------------------------------------------
+cp vk.icon.icns VisualKitchen.app/Contents/Resources/
+cp Info.plist VisualKitchen.app/Contents/
+
